692cad17106036f280ce2e4cd6a7a7cc281ce887
douglas-snyder <mistershake2099@gmail.com>
2022-06-08 10:42:35 -0700

    Small nomenclature change.

--- Assets/Scripts/Travelman.cs
+++ Assets/Scripts/Travelman.cs
@@ -24,7 +24,7 @@ public class Travelman : MonoBehaviour
     void DropdownAction()
     {
         // Get and handle new dropdown selection.
-        Debug.Log("DropdownAction");
+        Debug.Log("Dropdown_Action");
     }
 
 }
